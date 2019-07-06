d145e9ef6e2498df85bdc323f79738ef9c5d5a80
Kamil Kierski <cybernetic87@gmail.com>
2019-07-06 11:21:51 +0200

    new modifications

--- Tizen Webview Playground/Tizen_Webview_Playground/HtmlResponseParser.cs
+++ Tizen Webview Playground/Tizen_Webview_Playground/HtmlResponseParser.cs
@@ -7,22 +7,30 @@ namespace GAssist
     {
         public static string ParseHtmlResponse(string html)
         {
-            var bodyOld = "body{background:transparent;margin:0}";
-            var bodyNew =
-                "body{background:transparent;margin:0; transform: scale(0.5); transform - origin: 0 0;}";
+            //var bodyOld = "body{background:transparent;margin:0}";
+            //var bodyNew =
+            //    "body { background: transparent; margin: 0; -webkit - transform: scale(0.5); }";
+            var shadow = "<div class=\"popout-shadow\" id=\"assistant-shadow\"></div>";
+            var positionAbs = "position: absolute;";
 
-            return html.Replace(bodyOld, bodyNew);
+            var htmlOld = "html";
+            var htmlNew = "html {\n\tposition: relative;\n    max-width: 400px;\n    max-height: 400px;\n\t\n    height: 100%;\n\t}";
+
+            var popoutOld = "#popout{";
+            var popoutNew = "#popout{ -moz-transform: scale(0.4, 0.4); zoom: 0.4; zoom: 40%;";
+
+            return html.Replace(htmlOld, htmlNew).Replace(popoutOld, popoutNew).Replace(shadow, "").Replace(positionAbs, "");
         }
 
-        //public static string ParseHtmlResponse2(string html)
-        //{
-        //    var flex = "display: -webkit-flex";
-        //    var wrap =
-        //        "display: -webkit-flex;\nheight: 360;\nwidth: 360;\n-webkit-flex-wrap: wrap;\n-webkit-flex-shrink: 1;";
+        public static string ParseHtmlResponse2(string html)
+        {
+            var flex = "display: -webkit-flex";
+            var wrap =
+                "display: -webkit-flex;\nheight: 360;\nwidth: 360;\n-webkit-flex-wrap: wrap;\n-webkit-flex-shrink: 1;";
 
-        //    var parsed = html.Replace(flex, wrap);
-        //    Log.Debug("HTMLPARSER", parsed);
-        //    return parsed;
-        //}
+            var parsed = html.Replace(flex, wrap);
+            Log.Debug("HTMLPARSER", parsed);
+            return parsed;
+        }
     }
 }
