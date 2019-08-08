66d93eeb1f6a6b682265a4301bae8c6b4cf987bf
LokiWuh <lokiwuh@gmail.com>
2019-08-07 21:50:46 -0500

    Update HtmlResponseParser.cs

--- Tizen Webview Playground/Tizen_Webview_Playground/HtmlResponseParser.cs
+++ Tizen Webview Playground/Tizen_Webview_Playground/HtmlResponseParser.cs
@@ -7,19 +7,22 @@ namespace GAssist
     {
         public static string ParseHtmlResponse(string html)
        {
-            //  var asisOld = "assistant-bar-content";
-            //  var asisNew = "assistant-bar-content{background-color: black;height: 0px !important; visibility: collapse !important; display: none !important;}";
-            //  var positionAbs = "position: fixed;";
+          //  var positionAbs = "position: fixed;";
+            var sugOld = "assistant_response";
+            var sugNew = "assistant_response{opacity: 0.0 !important;}";
+            var asisOld = "assistant-bar";
+            var asisNew = "assistant-bar{ visibility: collapse;}";
+            var shadow = "<div class=\"popout-shadow\" id=\"assistant-shadow\"></div>";
             var htmlOld = "html";
-            var htmlNew = "html{ background-color: black;}";
+            var htmlNew = "html{ background-color: rgb(24, 24, 24);padding-right:50px;}";
             var popoutOld = "#popout{";
-            var popoutNew = "#popout{ -moz-transform: scale(0.5, 0.5); zoom: 0.5; zoom: 50%;margin-top:50px;background-color: black;";
+            var popoutNew = "#popout{ -moz-transform: scale(0.5, 0.5); zoom: 0.5; zoom: 50%;margin-top:100px;padding-left:0px;background-color: rgb(24, 24, 24);";
             var assistbarOld = "#carousel-container{";
-            var assistbarNew = "#carousel-container{-moz-transform: scale(0.5, 0.5); zoom: 0.5; zoom: 30%;";
+            var assistbarNew = "#carousel-container{-moz-transform: scale(0.5, 0.5); zoom: 0.5; zoom: 100%;";
             var flex = "display:-webkit-flex;";
-            var wrap = "display: -webkit-flex;\nheight: 300px;\nwidth: 160%;\n-webkit-flex-wrap: wrap;\n-webkit-flex-shrink: 1; scale = 1, maximum - scale = 0;";
+            var wrap = "display: -webkit-flex;\nheight: 260px;\nwidth: 110%;\n-webkit-flex-wrap: wrap;\n-webkit-flex-shrink: 1; scale = 1, maximum - scale = 0;";
 
-            return html.Replace(popoutOld, popoutNew).Replace(flex, wrap).Replace(assistbarOld, assistbarNew).Replace(htmlOld, htmlNew);
+            return html.Replace(htmlOld, htmlNew).Replace(popoutOld, popoutNew).Replace(assistbarOld, assistbarNew).Replace(asisOld, asisNew).Replace(sugOld, sugNew).Replace(flex, wrap).Replace(shadow, "");
         }
 
     }
